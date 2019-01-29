77cc2c0f53c72144e9194b1994ba55b7446908a4
Daniel Ransom <ransomdaniell@yahoo.com>
2019-01-29 12:30:22 +0100

    fix config

--- config/index.js
+++ config/index.js
@@ -4,17 +4,63 @@ const convict = require('convict');
 
 const config = convict({
     log: {
-        name: process.env.LOG_NAME,
-        level: process.env.LOG_LEVEL
+        name: {
+            doc: 'Log name',
+            format: String,
+            default: 'AMQP-lib',
+            env: 'LOG_NAME'
+        },
+        level: {
+            doc: 'Log level',
+            format: String,
+            default: 'debug',
+            env: 'APP_ENV'
+        }
     },
     amqp: {
-        host: process.env.AMQP_HOST,
-        username: process.env.AMQP_USERNAME,
-        password: process.env.AMQP_PASSWORD,
+        host: {
+            doc: 'AMQP Host',
+            format: String,
+            default: null,
+            env: 'AMQP_HOST'
+        },
+        username: {
+            doc: 'AMQP Username',
+            format: String,
+            default: null,
+            env: 'AMQP_USER'
+        },
+        password: {
+            doc: 'AMQP Password',
+            format: String,
+            default: null,
+            env: 'AMQP_PASSWORD'
+        },
+        port: {
+            doc: 'AMQP Port',
+            format: 'port',
+            default: 5672,
+            env: 'AMQP_PORT'
+        },
         retry: {
-            maxTries: process.env.AMQP_RETRY_MAX_TRIES || 10,
-            interval: process.env.AMQP_RETRY_INTERVAL || 1000,
-            backoff: process.env.AMQP_RETRY_BACKOFF || 2
+            maxTries: {
+                doc: 'Number of retries',
+                format: int,
+                default: 10,
+                env: 'AMQP_RETRIES'
+            },
+            interval: {
+                doc: 'Retry interval',
+                format: int,
+                default: 1000,
+                env: 'AMQP_RETRY_INTERVAL'
+            },
+            backoff: {
+                doc: 'Retry backoff',
+                format: int,
+                default: 2,
+                env: 'AMQP_RETRY_BACKOFF'
+            }
         }
     }
 });
