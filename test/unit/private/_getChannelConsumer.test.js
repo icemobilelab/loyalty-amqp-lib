9222d961f71d30c38aa1c34463c415dbba85dce2
Thijs Gadiot <thijs@gadiot.net>
2019-02-05 13:33:08 +0100

    add getChannelConsumer unit tests

---
+++ test/unit/private/_getChannelConsumer.test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { expect } = require('chai');
+const { EventEmitter } = require('events');
+const rewire = require('rewire');
+const AMQP = rewire('../../../lib/amqp-base');
+const sinon = require('sinon');
+const mockLogger = require('../../util/mock-logger');
+
+// inner function should emit event on base object
+
+
+
+describe('_getChannelConsumer', () => {
+
+    const _getChannelConsumer = AMQP.__get__('_getChannelConsumer');
+
+    it('should return a function', function (done) {
+        expect(typeof _getChannelConsumer()).to.equal('function');
+        done();
+    });
+
+    it('returned function should call emit on base', function (done) {
+        const callback = sinon.fake();
+        const base = { emit: callback };
+        const channelConsumer = _getChannelConsumer(base, mockLogger);
+        const message = 'hello there';
+        const data = { fields: [], content: message };
+        channelConsumer(data);
+        expect(callback.callCount).to.equal(1);
+        expect(callback.getCall(0).args[1]).to.equal(message);
+        done();
+    });
+
+});
