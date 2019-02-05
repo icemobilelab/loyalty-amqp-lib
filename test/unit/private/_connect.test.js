db03fa429251bef01068f414b9a1f51b13f869eb
Thijs Gadiot <thijs@gadiot.net>
2019-02-05 16:08:08 +0100

    cleanup

--- test/unit/private/_connect.test.js
+++ test/unit/private/_connect.test.js
@@ -13,38 +13,35 @@ describe('_connect', () => {
     const connection = new EventEmitter();
     const mitty = new EventEmitter();
 
-    AMQP.__set__('_createConnection', async () => {
+    AMQP.__set__('_createConnection', () => {
         return Promise.resolve(connection);
     });
-    AMQP.__set__('_connectionCloseHandler', function (...args) {
+    AMQP.__set__('_connectionCloseHandler', () => {
         mitty.emit('handledClose');
     });
 
     let base;
     beforeEach(function () {
+        this.timeout(2000);
         base = new AMQP.AMQP(constructor(config));
     });
 
     it('Successfully connects', async function () {
-        this.timeout(2000);
         const conn = await _connect(base);
         expect(conn, 'returns created connection').to.be.eql(connection);
     });
 
     it('Emits connect event', function (done) {
-        this.timeout(2000);
         base.once('connect', done);
         _connect(base);
     });
 
     it('Emits reconnect event', function (done) {
-        this.timeout(2000);
         base.once('reconnect', done);
         _connect(base, true);
     });
 
     it('Calls close handler on connection close', function (done) {
-        this.timeout(2000);
         mitty.once('handledClose', done);
         _connect(base)
             .then((conn) => {
