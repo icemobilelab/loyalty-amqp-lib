75ea67be35659276fc96172ff85160a92378c4c7
Unknown <ransomdaniell@yahoo.com>
2019-02-05 11:57:17 +0100

    Mock _connect

--- test/integration/private/_getConnection.test.js
+++ test/integration/private/_getConnection.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { expect } = require('chai');
+const { EventEmitter } = require('events');
 const rewire = require('rewire');
 const AMQP = rewire('../../../lib/amqp-base');
 const config = require('../../config');
@@ -8,24 +9,29 @@ const constructor = require('../../util/constructor');
 
 describe('_getConnection', () => {
 
-
     async function _getConnection(base) {
         const inner = AMQP.__get__('_getConnection');
         const res = await inner(base);
         return res;
     }
 
+    AMQP.__set__('_connect', async (instance) => {
+        base._connection = new EventEmitter();
+    });
+
+    let base = new AMQP.AMQP(constructor(config));
+    beforeEach(() => {
+        base = new AMQP.AMQP(constructor(config));
+    });
+
     it('Successfully gets a connection', async function () {
         this.timeout(10000);
-        const base = new AMQP.AMQP(constructor(config));
 
-        const res = await _getConnection(base);
-        return res;
+        return await _getConnection(base);
     });
 
     it('Only creates a connection once', async function () {
         this.timeout(10000);
-        const base = new AMQP.AMQP(constructor(config));
 
         return Promise.race([
             _getConnection(base),
@@ -34,14 +40,13 @@ describe('_getConnection', () => {
         ]).then(conn => {
             expect(
                 base._connection,
-                'connection object only created once'
+                'first created connection matches final'
             ).is.eql(conn);
         });
     });
 
-    it('All created connections match the first', async function () {
+    it('All created connections match', async function () {
         this.timeout(10000);
-        const base = new AMQP.AMQP(constructor(config));
 
         return Promise.all([
             _getConnection(base),
@@ -55,39 +60,5 @@ describe('_getConnection', () => {
                 ).is.eql(conn);
             }
         });
-    });
-});
-describe('_setConnection', () => {
-
-
-    function _setConnection(base) {
-        const inner = AMQP.__get__('_setConnection');
-        const res = inner(base);
-        return res;
-    }
-
-    it('Successfully sets a connection', function () {
-        this.timeout(2000);
-
-        const testValue = 'test';
-        const base = new AMQP.AMQP(constructor(config));
-        console.log('[_setConnection:test] base._connection:', base._connection);
-        base._connection = 'test';
-        console.log('[_setConnection:test] base._connection:', base._connection);
-        _setConnection(base, testValue);
-        console.log('[_setConnection:test] base._connection:', base._connection);
-        expect(base._connection).to.be.eql(testValue);
-    });
-    it('Successfully sets a connection to undefined', function () {
-        this.timeout(2000);
-
-        const testValue = undefined;
-        const base = new AMQP.AMQP(constructor(config));
-        console.log('[_setConnection:test] base._connection:', base._connection);
-        base._connection = 'incorrect_value';
-        console.log('[_setConnection:test] base._connection:', base._connection);
-        _setConnection(base, testValue);
-        console.log('[_setConnection:test] base._connection:', base._connection);
-        expect(base._connection).to.be.eql(testValue);
     });
 });
