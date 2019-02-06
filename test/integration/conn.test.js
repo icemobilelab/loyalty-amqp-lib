e022acb4d23813f5420bcc7889cf37d24c0844e9
Unknown <ransomdaniell@yahoo.com>
2019-02-06 18:03:23 +0100

    Fix connection tests

--- test/integration/conn.test.js
+++ test/integration/conn.test.js
@@ -1,49 +1,155 @@
 'use strict';
 
 const { expect } = require('chai');
-const uuid = require('uuid');
+const Bluebird = require('bluebird');
+const { AMQPConsumer } = require('../../index');
 const rewire = require('rewire');
-const { AMQPConsumer } = rewire('../../index');
+const AMQP = rewire('../../lib/amqp-base');
 const config = require('../config');
 const queueOptions = require('../util/constructor');
 
 
-describe('Starting up and shutting down the queue', () => {
-    this.timeout(2000);
+describe('Starting up the queue', () => {
 
     let queue = new AMQPConsumer(queueOptions(config));
     beforeEach(() => {
         queue = new AMQPConsumer(queueOptions(config));
     });
 
-    afterEach((done) => {
-        Promise.resolve()
-            .then(() => queue.stop())
-            .then(() => queue.removeAllListeners())
-            .then(() => done())
-            .catch(err => done(err));
+    afterEach(async () => {
+        queue.removeAllListeners();
+        queue.stop();
     });
 
     it('Retries connection to the AMQP server with maxTries', function (done) {
-        //...
+        this.timeout(50000);
+
+        const maxTries = 1;
+        let options = queueOptions(config);
+        options.host = 'nowhere';
+        options.retry.maxTries = maxTries;
+        options.retry.interval = 2;
+        queue = new AMQPConsumer(options);
+
+        let count = 0;
+        const resolve = AMQP.__set__('amqp.connect', () => {
+            count++;
+            let err = new Error('ENOTFOUND');
+            err.code = 'ENOTFOUND';
+            // bluebird-retry requires bluebird promises
+            return Bluebird.reject(err);
+        });
+
+        queue.listen()
+            .catch(err => {
+                expect(err.code, 'Should thrown ENOTFOUND error').to.eql('ENOTFOUND');
+                expect(count, `Should retry connection ${maxTries} times`).to.eql(maxTries);
+                resolve();
+                done();
+            });
     });
 
-    it('Retries connection indefinitely to the AMQP server without maxTries', function (done) {
-        //...
-    });
+    it('Retries connection to the AMQP server indefinitely', function (done) {
+        this.timeout(4000);
 
-    it('Closes an open connection to AMQP', done => {
-        //...
+        const maxTries = -1;
+        let options = queueOptions(config);
+        options.host = 'nowhere';
+        options.retry.maxTries = maxTries;
+        options.retry.interval = 2;
+        queue = new AMQPConsumer(options);
+
+        const resolve = AMQP.__set__('amqp.connect', () => {
+            let err = new Error('ENOTFOUND');
+            err.code = 'ENOTFOUND';
+            // bluebird-retry requires bluebird promises
+            return Bluebird.reject(err);
+        });
+
+        queue.listen()
+            .catch(done);
+
+        setTimeout(() => {
+            resolve();
+            done();
+        }, 1500);
+    });
+});
+
+describe('Shutting down the queue', () => {
+    let queue = new AMQPConsumer(queueOptions(config));
+
+    it('Closes an open connection to AMQP', function (done) {
+        queue.once('close', () => {
+            expect(queue._connection).to.be.undefined;
+            done();
+        });
+        queue.listen()
+            .then(() => {
+                queue.stop();
+            });
     });
 });
 
 describe('Handles AMQP disconnects', () => {
+    const { _getChannel } = require('../../lib/amqp-base');
+
+    let queue = new AMQPConsumer(queueOptions(config));
+    beforeEach(() => {
+        queue = new AMQPConsumer(queueOptions(config));
+    });
+
+    it('Reconnects on Channel close event', function (done) {
+        queue.once('reconnect', done);
+        queue.listen()
+            .then(() => {
+                _getChannel(queue)
+                    .then(channel => {
+                        channel.close();
+                    });
+            });
+    });
+
+    it('Reconnects on Connection close event', function (done) {
+        this.timeout(50000);
+        // Use rewire to get un-exported function
+        const _getConnection = AMQP.__get__('_getConnection');
+        queue.once('reconnect', done);
+        queue.listen()
+            .then(() => {
+                _getConnection(queue, false)
+                    .then(conn => {
+                        conn.close();
+                    });
+            });
+    });
 
     it('Listens again on Channel close event', function (done) {
-        //...
+        queue.once('reconnect', () => {
+            queue.once('listen', done);
+        });
+        queue.listen()
+            .then(() => {
+                _getChannel(queue)
+                    .then(channel => {
+                        channel.close();
+                    });
+            });
     });
 
     it('Listens again on Connection close event', function (done) {
-        //...
+        this.timeout(5000);
+        // Use rewire to get un-exported function
+        const _getConnection = AMQP.__get__('_getConnection');
+        queue.once('reconnect', () => {
+            queue.once('listen', done);
+        });
+        queue.listen()
+            .then(() => {
+                _getConnection(queue, false)
+                    .then(conn => {
+                        conn.emit('close', new Error());
+                    });
+            });
     });
 });
