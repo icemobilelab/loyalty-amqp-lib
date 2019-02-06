6574ace113a6b3d16ca77defd9c73a0283257256
Daniel Ransom <ransomdaniell@yahoo.com>
2019-02-06 10:22:53 +0100

    replace test

--- test/integration/consumer.test.js
+++ test/integration/consumer.test.js
@@ -3,11 +3,13 @@
 const { expect } = require('chai');
 const rewire = require('rewire');
 const { AMQPConsumer } = rewire('../../index');
+const AMQP = rewire('../../lib/amqp-base');
 const config = require('../config');
 const queueOptions = require('../util/constructor');
 
 describe('Listening to a queue', () => {
 
+    let _getChannel = AMQP.__get__('_getChannel');
     let consumer = new AMQPConsumer(queueOptions(config));
 
     after(() => {
@@ -19,8 +21,13 @@ describe('Listening to a queue', () => {
         consumer.listen();
     });
 
-    it('Handles errors when listening to a queue', function (done) {
-        done(new Error('TBD'));
+    it.only('Handles errors when listening to a queue', function (done) {
+        consumer.once('reconnect', done);
+        consumer.listen();
+        _getChannel(consumer)
+            .then(channel => {
+                channel.close();
+            });
     });
 
     it('Listens to message events', function (done) {
