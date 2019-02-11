d0ba75202e499a9de5f62c08b500887829a8c2b2
Thijs Gadiot <thijs@gadiot.net>
2019-02-11 10:02:21 +0100

    add queue options etc to test constructor

--- test/util/constructor.js
+++ test/util/constructor.js
@@ -8,6 +8,10 @@ module.exports = function queueOptions(config) {
         username: config.get('amqp.username'),
         password: config.get('amqp.password'),
         retry: config.get('amqp.retry'),
+        queue: config.get('amqp.queue'),
+        exchange: config.get('amqp.exchange'),
+        exchangeType: config.get('amqp.exchangeType'),
+        route: config.get('amqp.route'),
         logger
     };
 };
